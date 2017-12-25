e407074681f15ed01e9ac7f00560598638cc1b7c
Ghusar <ghusarhardikr@gmail.com>
2017-12-25 14:04:14 +0530

    basic add remove functionality using react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TodoList from "./TodoList";
+
+var container = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <TodoList />
+    </div>,
+    container
+);
